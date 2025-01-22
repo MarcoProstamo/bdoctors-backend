7a792736e56f7a8da5e851ffd74be49dfe97ea62
MarcoProstamo <Marcopros1201@gmail.com>
2025-01-22 12:26:48 +0100

    Feat → Show Doctors and Theirs Reviews

--- controllers/doctorsController.js
+++ controllers/doctorsController.js
@@ -4,12 +4,63 @@ const doctorsController = {
   index(req, res) {
     const sqlIndex = `SELECT doctors.id, doctors.name, doctors.surname, doctors.medical_specialization FROM doctors;`;
     connection.query(sqlIndex, (err, results) => {
-      if (err) throw err;
+      if (err) res.status(500).json({ status: "KO", message: err.sqlMessage });
       res.json(results);
     });
   },
   show(req, res) {
-    res.send("show");
+    const doctorId = parseInt(req.params.id);
+    if (isNaN(doctorId) || doctorId < 1)
+      return res
+        .status(400)
+        .json({ status: "KO", message: "Invalid Parameter" });
+    const sqlShowDoctors = `SELECT doctors.id, 
+       doctors.name, 
+       doctors.surname, 
+       doctors.email, 
+       doctors.cellphone_number, 
+       doctors.address, 
+       doctors.medical_specialization
+       FROM doctors
+       WHERE doctors.id = ?;
+       `;
+
+    const sqlShowReviews = `
+      SELECT reviews.id,
+        reviews.name,
+        reviews.vote,
+        reviews.text,
+        reviews.created_at,
+        reviews.updated_at
+      FROM reviews
+      INNER JOIN doctors 
+      ON doctors.id = reviews.doctor_id
+      WHERE doctors.id = ?;
+    `;
+
+    connection.query(sqlShowDoctors, [doctorId], (err, results) => {
+      if (err)
+        return res.status(500).json({ status: "KO", message: err.sqlMessage });
+      if (!results.length)
+        return res
+          .status(404)
+          .json({ status: "KO", message: "Doctor Not Found" });
+
+      const doctor = results[0];
+
+      connection.query(sqlShowReviews, [doctorId], (err, results) => {
+        if (err)
+          return res
+            .status(500)
+            .json({ status: "KO", message: err.sqlMessage });
+        if (!results.length)
+          return res
+            .status(404)
+            .json({ status: "KO", message: "Reviews Not Found" });
+        doctor.reviews = results;
+        res.json(doctor);
+      });
+    });
   },
   store(req, res) {
     res.send("store");
