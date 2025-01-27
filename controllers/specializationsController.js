30d8c52a6a160a4540106211ffc0bc3d8d14a85d
MarcoProstamo <Marcopros1201@gmail.com>
2025-01-27 16:12:11 +0100

    Refactor → Removed Logs

--- controllers/specializationsController.js
+++ controllers/specializationsController.js
@@ -5,12 +5,7 @@ const specializationsController = {
     const sqlIndex =
       "SELECT id, specialization, icon_tag FROM bdoctors.specializations;";
 
-    console.log(req);
-
     connection.query(sqlIndex, (err, results) => {
-      console.log(err);
-      console.log(results);
-
       if (err)
         return res.status(500).json({ status: "KO", message: err.sqlMessage });
 
